17400cda5add93aae49ce6964c5db12b8c0dc049
WP-Studio01 <98379477+WP-Studio01@users.noreply.github.com>
2023-11-05 09:24:50 +0800

    自动更新

--- main.cpp
+++ main.cpp
@@ -8,10 +8,19 @@
 #include "cui/text.hpp"
 #include "cui/textbox.hpp"
 using namespace std;
+using namespace Controls;
 int main()
 {
 	Render::Start(26,100);
 	Input::Start();
+	Button btn1;
+	btn1.Name="BTN1";
+	btn1.CoordX=30,btn1.CoordY=10;
+	btn1.SizeX=40,btn1.SizeY=1;
+	btn1.ForgColor=15,btn1.BackColor=0;
+	btn1.MouseColor=7;
+	btn1.Caption="Start";
+	btn1.Join();
 	while(1)
 	{
 	}
