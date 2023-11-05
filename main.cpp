d71512b84fd22ad2d9daeb3689c63a202654c350
WP-Studio01 <98379477+WP-Studio01@users.noreply.github.com>
2023-11-05 09:39:48 +0800

    自动更新

--- main.cpp
+++ main.cpp
@@ -7,18 +7,21 @@
 #include "cui/button.hpp"
 #include "cui/text.hpp"
 #include "cui/textbox.hpp"
+#define alert(str) MessageBox(NULL,str,"alert",MB_OK)
 using namespace std;
 using namespace Controls;
 void CreateButton(Button & btn,string id,string text,
 	short cx,short cy,short sx,short sy,
 	short fc,short bc,short mc
 );
+void BTN1LeftClick();
 int main()
 {
 	Render::Start(26,100);
 	Input::Start();
 	Button btn1,btn2;
 	CreateButton(btn1,"BTN1","start",35,10,30,1,15,0,7);
+	btn1.LeftClicked=BTN1LeftClick;
 	CreateButton(btn2,"BTN2","exit", 35,14,30,1,15,0,7);
 	btn1.Join();
 	btn2.Join();
@@ -38,5 +41,9 @@ void CreateButton(Button & btn,string id,string text,
 	btn.ForgColor=fc,btn.BackColor=bc;
 	btn.MouseColor=mc;
 	btn.Caption=text;
+}
+void BTN1LeftClick()
+{
+	alert("Started");
 }
 
